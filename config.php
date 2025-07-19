8b5bdc1c693f1e5a154ae568067215719f56ea13
marioillan <marioillan12@gmail.com>
2025-07-19 23:32:01 +0200

    Subida de imagenes

--- config.php
+++ config.php
@@ -1,17 +1,12 @@
 <?php
-$host = 'localhost';
-$dbname = 'museoDB';
-$user = 'postgres';
-$password = 'Mario321';
+$host = "localhost";
+$user = "root";         
+$password = "";         
+$dbname = "museoDB";
 
 $conn = new mysqli($host, $user, $password, $dbname);
-$conn->set_charset("utf8");
 
-try {
-    $conn = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    // echo "Conexión correcta a PostgreSQL.";
-} catch (PDOException $e) {
-    echo "Error de conexión: " . $e->getMessage();
+if ($conn->connect_error) {
+    die("Error de conexión: " . $conn->connect_error);
 }
 ?>
